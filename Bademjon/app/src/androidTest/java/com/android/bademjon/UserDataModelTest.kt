b097e55ce273ce963a329ca4f1ba48a0236c3007
alirrah <alir.rah81@gmail.com>
2024-05-31 20:27:39 +0330

    complete UserDataModel test

--- Bademjon/app/src/androidTest/java/com/android/bademjon/UserDataModelTest.kt
+++ Bademjon/app/src/androidTest/java/com/android/bademjon/UserDataModelTest.kt
@@ -33,5 +33,59 @@ class UserDataModelTest {
         assertEquals(newHeight.toString(), userDataModel.height.value)
     }
 
-    // Repeat the same test format for other update functions like updateAge, updateWrist, updateRate, updateGender, updateDisease
+    @Test
+    fun testUpdateWeight() {
+        val userDataModel = UserDataModel()
+        val newWeight = 70
+        userDataModel.updateWeight(newWeight)
+        assertEquals(newWeight.toString(), userDataModel.weight.value)
+    }
+
+    @Test
+    fun testUpdateHeight() {
+        val userDataModel = UserDataModel()
+        val newHeight = 170
+        userDataModel.updateHeight(newHeight)
+        assertEquals(newHeight.toString(), userDataModel.height.value)
+    }
+
+    @Test
+    fun testUpdateAge() {
+        val userDataModel = UserDataModel()
+        val newAge = 25
+        userDataModel.updateAge(newAge)
+        assertEquals(newAge.toString(), userDataModel.age.value)
+    }
+
+    @Test
+    fun testUpdateWrist() {
+        val userDataModel = UserDataModel()
+        val newWrist = 16
+        userDataModel.updateWrist(newWrist)
+        assertEquals(newWrist.toString(), userDataModel.wrist.value)
+    }
+
+    @Test
+    fun testUpdateRate() {
+        val userDataModel = UserDataModel()
+        val newRate = 80
+        userDataModel.updateRate(newRate)
+        assertEquals(newRate.toString(), userDataModel.rate.value)
+    }
+
+    @Test
+    fun testUpdateGender() {
+        val userDataModel = UserDataModel()
+        val newGender = 1 // assuming 1 as male
+        userDataModel.updateGender(newGender)
+        assertEquals(newGender.toString(), userDataModel.gender.value)
+    }
+
+    @Test
+    fun testUpdateDisease() {
+        val userDataModel = UserDataModel()
+        val newDisease = 2 // assuming 2 as a specific disease code
+        userDataModel.updateDisease(newDisease)
+        assertEquals(newDisease.toString(), userDataModel.disease.value)
+    }
 }
